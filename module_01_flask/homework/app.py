596e04ac22b791b04309e01e62178e0292730a55
Denis Varjuhin <denisvaruxin@mail.ru>
2023-12-28 19:59:31 +0300

    апдейт по домашней работе

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,42 +1,64 @@
-import datetime
+from datetime import datetime, timedelta
+import os
+import random
+import re
 from flask import Flask
+
 
 app = Flask(__name__)
 
+CAT_LIST = [
+    'корниш-рекс', 'русская голубая',
+    'шотландская вислоухая', 'мейн-кун', 'манчкин'
+]
+CAR_LIST = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def word_return():
+    text_path = os.path.join(BASE_DIR, 'war_and_peace.txt')
+    with open('war_and_peace.txt', 'r', encoding='utf-8') as raw_text:
+        text = raw_text.read()
+        word_list = re.findall(r'\b(\w+)\b', text)
+        return random.choice(word_list)
+
 
 @app.route('/hello_world')
-def test_function():
-    pass
+def hello_world():
+    return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-    pass
+def cars():
+    return ' '.join(CAR_LIST)
 
 
 @app.route('/cats')
-def test_function():
-    pass
+def cats():
+    return random.choice(CAT_LIST)
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def get_time_now():
+    return '«Точное время: {current_time}»'.format(current_time=datetime.now())
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def get_time_future():
+    current_time_after_hour = datetime.now() + timedelta(hours=1)
+    return f'Точное время через час будет {current_time_after_hour}»'
 
 
 @app.route('/get_random_word')
-def test_function():
-    pass
+def get_random_word():
+    return word_return()
 
 
 @app.route('/counter')
-def test_function():
-    pass
+def counter():
+    counter.visits += 1
+    return str(counter.visits)
+counter.visits = 0
 
 
 if __name__ == '__main__':
