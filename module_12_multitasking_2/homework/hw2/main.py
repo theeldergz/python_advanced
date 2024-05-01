3f4b3884bc34d3c21420cc100cb10709acbddb1d
varjuhind <theeldergodzagrej@yandex.ru>
2024-05-01 21:58:59 +0300

    апдейт по 2 заданию

--- module_12_multitasking_2/homework/hw2/main.py
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -1,10 +1,22 @@
+import shlex
+import subprocess
+import sys
+
+
 def process_count(username: str) -> int:
-    # количество процессов, запущенных из-под
-    # текущего пользователя username
-    pass
+    # cmd = shlex.split(f'ps -u {username} | wc -l ')
+    cmd = ['ps', '-u', username, '-o', 'pid']
+    pid_list = subprocess.run(cmd, capture_output=True)
+    result = len(pid_list.stdout.decode().splitlines()) - 1
+    return result
 
 
 def total_memory_usage(root_pid: int) -> float:
-    # суммарное потребление памяти древа процессов
-    # с корнем root_pid в процентах
-    pass
+    cmd = ['ps', '--ppid', str(root_pid), '-o', '%mem']
+    mem_list = subprocess.run(cmd, capture_output=True).stdout.decode().splitlines()
+    result = sum(map(float, mem_list[1:]))
+    return result
+
+
+print(process_count('varjuhind'))
+print(total_memory_usage(1))
