062b5198afc202c0cd8b13f76d2421eda44e1d6f
Denis Varjuhin <denisvaruxin@mail.ru>
2024-05-28 21:37:42 +0300

    апдейт по 1-4 заданию

--- module_14_mvc/homework/models.py
+++ module_14_mvc/homework/models.py
@@ -10,13 +10,25 @@ DATA: List[dict] = [
 
 class Book:
 
-    def __init__(self, id: int, title: str, author: str) -> None:
+    def __init__(self, id: [int, None], title: str, author: str, view_count: int) -> None:
         self.id: int = id
         self.title: str = title
         self.author: str = author
+        self.view_count: int = view_count
 
     def __getitem__(self, item: str) -> Any:
         return getattr(self, item)
+
+
+def _create_view_counter_column():
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            ALTER TABLE `table_books`
+            ADD view_count INT;
+            """
+        )
 
 
 def init_db(initial_records: List[dict]) -> None:
@@ -36,8 +48,7 @@ def init_db(initial_records: List[dict]) -> None:
                 CREATE TABLE `table_books` (
                     id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     title TEXT, 
-                    author TEXT, 
-                )
+                    author TEXT)
                 """
             )
             cursor.executemany(
@@ -57,7 +68,95 @@ def get_all_books() -> List[Book]:
         cursor: sqlite3.Cursor = conn.cursor()
         cursor.execute(
             """
-            SELECT * from `table_books`
+            SELECT * FROM `table_books`
             """
         )
+        return [Book(*row) for row in cursor.fetchall()]
+
+
+def get_all_books_for_author(author: str) -> List:
+    with sqlite3.connect('table_books.db') as conn:
+        author = f'%{author}%'
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            SELECT * FROM `table_books`
+                WHERE `author` LIKE ?
+            
+            """
+        )
+        cursor.execute(sql_request, (author,))
+        return [Book(*row) for row in cursor.fetchall()]
+
+
+def add_new_book(new_book: Book) -> None:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            INSERT INTO `table_books`('title', 'author') VALUES (?, ?)
+            """
+        )
+
+        cursor.execute(sql_request, (new_book['title'], new_book['author']))
+
+
+def get_count_books() -> int:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            SELECT COUNT(id) FROM `table_books`
+            """
+        )
+
+        cursor.execute(sql_request)
+        return cursor.fetchone()[0]
+
+
+def add_view_to_book(book_id: int):
+    with sqlite3.connect('table_books.db') as conn:
+        cur_count = get_view_for_book(book_id) + 1
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            UPDATE `table_books` SET `view_count` = ?
+                WHERE `id` = ?
+            """
+        )
+
+        cursor.execute(sql_request, (cur_count, book_id,))
+
+
+def get_view_for_book(book_id: int):
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            SELECT `view_count` FROM `table_books`
+                WHERE `id` = ?
+            """
+        )
+
+        cursor.execute(sql_request, (book_id,))
+        count = cursor.fetchone()
+
+        if count[0] is None:
+            count = 0
+            return count
+        return count[0]
+
+
+def get_book_info(book_id: int):
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_request = (
+            """
+            SELECT * FROM `table_books`
+                WHERE `id` = ?
+            """
+        )
+
+        cursor.execute(sql_request, (book_id,))
+
         return [Book(*row) for row in cursor.fetchall()]
--- module_14_mvc/homework/routes.py
+++ module_14_mvc/homework/routes.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
-from typing import List
+from typing import List, Tuple
 
-from models import init_db, get_all_books, DATA
+from flask import Flask, render_template, request
+
+from wtforms import StringField
+from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm
+
+from models import init_db, get_all_books, add_new_book, DATA,\
+    Book, get_all_books_for_author, get_count_books,\
+    get_book_info, get_view_for_book, add_view_to_book
 
 app: Flask = Flask(__name__)
+
+
+class UpdateBooksForm(FlaskForm):
+    title = StringField(validators=[InputRequired()])
+    author = StringField(validators=[InputRequired()])
+
+
+class GetAuthorBooks(FlaskForm):
+    author = StringField(validators=[InputRequired()])
 
 
 def _get_html_table_for_books(books: List[dict]) -> str:
@@ -23,8 +39,8 @@ def _get_html_table_for_books(books: List[dict]) -> str:
 """
     rows: str = ''
     for book in books:
-        rows += '<tr><td>{id}</tb><td>{title}</tb><td>{author}</tb></tr>'.format(
-            id=book['id'], title=book['title'], author=book['author'],
+        rows += '<tr><td>{id}</tb><td>{title}</tb><td>{author}</tb><td>{count}</tb></tr>'.format(
+            id=book['id'], title=book['title'], author=book['author'],count=book['view_count']
         )
     return table.format(books_rows=rows)
 
@@ -34,14 +50,36 @@ def all_books() -> str:
     return render_template(
         'index.html',
         books=get_all_books(),
+        number=get_count_books()
     )
 
 
-@app.route('/books/form')
+@app.route('/books/form', methods=['GET', 'POST'])
 def get_books_form() -> str:
+    UpdateBooksForm()
+    if request.method == 'POST':
+        new_book = Book(id=None, title=request.form['book_title'], author=request.form['author_name'], view_count=0)
+        add_new_book(new_book)
     return render_template('add_book.html')
+
+
+@app.route('/books/search', methods=['GET', 'POST'])
+def search_book() -> str:
+    form = GetAuthorBooks()
+    if request.method == 'POST':
+        author = form.author.data
+        return render_template('search_result.html', books=get_all_books_for_author(author=author))
+    return render_template('books_for_author.html', form=form)
+
+
+@app.route('/books/<int:book_id>', methods=['GET'])
+def about_book(book_id: int):
+    add_view_to_book(book_id)
+    return render_template('about_book.html', books=get_book_info(book_id=book_id), book_id=book_id)
 
 
 if __name__ == '__main__':
     init_db(DATA)
+    app.config['WTF_CSRF_ENABLED'] = False
     app.run(debug=True)
+
